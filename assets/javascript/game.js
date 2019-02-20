1c64c861028c36f23f36e3ef75fd3d0f7cc5d97a
Druidan <44009123+Druidan@users.noreply.github.com>
2019-02-19 18:04:00 -0600

    final build, I think. At least before homework is due.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,6 +16,11 @@ $(document).ready(function(){
     var gameOn = false; //turns most game functions on or off.
     var opponentSelected = false; //Combat Start Condition
     var playerDefeated = false; //Loss Condition
+    var audioPlaying = false; //audio on setting
+
+    var opponentDeath; //capturing death sound for use in combat.
+    var playerLoss; //capturing loss sound for use on player loss.
+    var playerWin; //capturing win sound for use in player win.
 
     const cards = {
         1: $(".card1"),
@@ -34,50 +39,86 @@ $(document).ready(function(){
             attack: 7,
             counterAttack: 7,
             hp: 11,
-            id: "anakinCard"},
+            id: "anakinCard",
+            selected: "assets/sounds/anakin_intro.mp3",
+            death: "assets/sounds/anakin_death.mp3",
+            lose: "assets/sounds/anakin_lose.mp3",
+            win: "assets/sounds/anakin_win.mp3"},
         2: {
             name: "Darth Maul",
             attack: 5,
             counterAttack: 5,
             hp: 1,
-            id: "maulCard"},
+            id: "maulCard",
+            selected: "assets/sounds/maul_intro.mp3",
+            death: "assets/sounds/maul_death.mp3",
+            lose: "assets/sounds/maul_lose.mp3",
+            win: "assets/sounds/maul_win.mp3"},
         3: {
             name: "Darth Vader",
             attack: 8,
             counterAttack: 8,
             hp: 31,
-            id: "vaderCard"},
+            id: "vaderCard",
+            selected: "assets/sounds/vader_intro.mp3",
+            death: "assets/sounds/vader_death.mp3",
+            lose: "assets/sounds/vader_lose.mp3",
+            win: "assets/sounds/vader_win.mp3"},
         4: {
             name: "Emperor Palpatine",
             attack: 10,
             counterAttack: 10,
             hp: 135,
-            id: "emperorCard"},
+            id: "emperorCard",
+            selected: "assets/sounds/emperor_selected.mp3",
+            opponentIntro: "assets/sounds/emperor_intro.mp3",
+            death: "assets/sounds/emperor_death.mp3",
+            lose: "assets/sounds/emperor_lose.mp3",
+            win: "assets/sounds/emperor_win.mp3"},
         5: {
             name: "Luke Skywalker",
             attack: 7,
             counterAttack: 7,
             hp: 5,
-            id: "lukeCard"},
+            id: "lukeCard",
+            selected: "assets/sounds/luke_intro.mp3",
+            death: "assets/sounds/luke_death.mp3",
+            lose: "assets/sounds/luke_lose.mp3",
+            win: "assets/sounds/luke_win.mp3"},
         6: {
             name: "Mace Windu",
             attack: 8,
             counterAttack: 8,
             hp: 18,
-            id: "winduCard"},
+            id: "winduCard",
+            selected: "assets/sounds/windu_intro.mp3",
+            death: "assets/sounds/windu_death.mp3",
+            lose: "assets/sounds/windu_lose.mp3",
+            win: "assets/sounds/windu_win.mp3"},
         7: {
             name: "Yoda",
             attack: 9,
             counterAttack: 9,
             hp: 57,
-            id: "yodaCard"},
+            id: "yodaCard",
+            selected: "assets/sounds/yoda_selected.mp3",
+            death: "assets/sounds/anakin_death.mp3",
+            lose: "assets/sounds/anakin_lose.mp3",
+            win: "assets/sounds/anakin_win.mp3"},
         8: {
             name: "Obi Wan Kenobi",
             attack: 6,
             counterAttack: 6,
             hp: 2,
-            id: "obiWanCard"},
+            id: "obiWanCard",
+            selected: "assets/sounds/obi_intro.mp3",
+            death: "assets/sounds/obi_death.mp3",
+            altDeath: "assets/sounds/obi_death_v_vader.mp3",
+            lose: "assets/sounds/obi_lose.mp3",
+            win: "assets/sounds/obi_win.mp3"},
     };
+
+
 
 //Use some For Loops to populate the character cards with their relevant statistics.
     //The engine that increases the number of characters at the start of the game. This will make it easier to add future characters.
@@ -104,9 +145,10 @@ $(document).ready(function(){
 
 //On click formula for selecting the player's character and opponent characters.
     characters.on("click", function(){
-        if (gameOn === false || opponentSelected === true){
+        if (gameOn === false || opponentSelected === true || audioPlaying === true){
             return false;
         }
+        const startFight = new sound("assets/sounds/ignite_saber.mp3");
         $(".command-text").text("Choose Your First Opponent!");
         $(".challengers").removeClass("buryIt");
         let clickedCharacter = $(this);
@@ -130,10 +172,18 @@ $(document).ready(function(){
             if (starWarsCharacters[property1].id === characterID) {
                 playerHP = starWarsCharacters[property1].hp;
                 playerPower = starWarsCharacters[property1].attack;
+                playerSelected = new sound(starWarsCharacters[property1].selected);
+                playerLoss = new sound(starWarsCharacters[property1].lose);
+                playerWin = new sound(starWarsCharacters[property1].win);
             };
         };
         var playerHP;
         var playerPower;
+        var playerSelected;
+        playerSelected.play();
+        playerSelected.onPlay = {
+
+        }
             $(".challengers").text("Your Enemies"); 
             $("#my-hp").removeClass("col-0").addClass("col-3").append("<h3 class='myHP'>My HP</h3>");
             $(".myHP").append("<p class='HP'></p>");
@@ -145,7 +195,7 @@ $(document).ready(function(){
             $(".power").text(playerPower);
         } else { 
             if (clickedCharacter.hasClass("opponentCard")) {
-                if (playerDefeated === true || opponentSelected === true){
+                if (playerDefeated === true || opponentSelected === true || audioPlaying === true){
                     return false;
                 }
                 opponentSelected = true;
@@ -153,6 +203,7 @@ $(document).ready(function(){
                 let opponentID = $(this).attr("id");
                 let combatZone = $(".combatant-zone");
                 let powerAdapter;
+                startFight.play();
                 for (var property1 in starWarsCharacters) {
                     if (starWarsCharacters[property1].id === $(".playerCard").attr("id")) {
                         powerAdapter = starWarsCharacters[property1].hp;
@@ -169,11 +220,23 @@ $(document).ready(function(){
                         opponentHP = starWarsCharacters[property1].hp;
                         opponentPower = starWarsCharacters[property1].attack;
                         opponentCounterAttack = starWarsCharacters[property1].counterAttack;
+                        if (characterID === "obiWanCard" && starWarsCharacters[property1].id === "vaderCard"){
+                            opponentDeath = new sound(starWarsCharacters[property1].altDeath);
+                        } else {
+                            opponentDeath = new sound(starWarsCharacters[property1].death);
+                        }
+                        if (starWarsCharacters[property1].id === "emperorCard"){
+                            opponentSelected = new sound(starWarsCharacters[property1].opponentIntro);
+                        } else {
+                            opponentIntro = new sound(starWarsCharacters[property1].selected);
+                        }
                     };
                 };
                 var opponentHP;
                 var opponentPower;
                 var opponentCounterAttack;
+                var opponentIntro;
+                opponentIntro.play();
                 $("#opponent-hp").append("<h3 class='opponentHP'>Their HP</h3>");
                 $(".opponentHP").append("<p class='oHP'></p>");
                 $(".oHP").text(opponentHP * (characterTotal - defeatedEnemies));
@@ -181,6 +244,7 @@ $(document).ready(function(){
                 $(".opponentPower").append("<p class='opower'></p>");
                 $(".opower").text(Math.round(opponentPower + (powerAdapter/(3+(characterTotal-defeatedEnemies)))));
                 $(".command-text").text("Clash with Your Enemy!");
+                console.log(gameOn);
                 if ($("#my-power").hasClass("hasMultiplier") === false) {
                     $("#my-power").addClass("hasMultiplier").append("<h3 class='myMultiplier'>Multiplier</h3>");
                     //$("#my-power");
@@ -195,9 +259,12 @@ $(document).ready(function(){
 
 //Now we set up combat through the Clash Button.
     clashButton.click(function(){
-        if (gameOn === false || opponentSelected === false){
+        if (gameOn === false || opponentSelected === false || audioPlaying === true){
             return false;
         }
+        const clashSounds = ["assets/sounds/clash1.mp3", "assets/sounds/clash2.mp3", "assets/sounds/clash3.mp3", "assets/sounds/clash4.mp3", "assets/sounds/clash5.mp3", "assets/sounds/clash6.mp3", "assets/sounds/clash7.mp3",];
+        const fightSound = new sound(clashSounds[Math.floor(Math.random()*clashSounds.length)]);
+        fightSound.play();
         playerAttack = $(".power").text() * powerMultiplier;
         opponentHP = $(".oHP").text()-playerAttack;
         $(".oHP").text(opponentHP);
@@ -208,6 +275,7 @@ $(document).ready(function(){
         $(".HP").text(PlayerHP);
         if ($(".HP").text() <= 0){
             gameOn = false;
+            playerLoss.play();
             const menuZone = $(".menuContent");
             const lossMenu = $("#lossMenu");
             const gameHead = $("#gameHeader");
@@ -223,6 +291,7 @@ $(document).ready(function(){
         if (opponentHP <= 0) {
             let defeatedOpponent = $(".currentCombatant");
             let defeatedID = defeatedOpponent.attr("id");
+            opponentDeath.play();
             defeatedOpponent.removeClass("currentCombatant").addClass("defeatedOpponent");
             waitingRoom.append(defeatedOpponent);
             $(".current-opponent-title").addClass("buryIt");
@@ -235,6 +304,7 @@ $(document).ready(function(){
             opponentSelected = false;
             if (defeatedEnemies === characterTotal) {
                 gameOn = false;
+                playerWin.play();
                 ++winCounter;
                 const menuZone = $(".menuContent");
                 const winMenu = $("#winMenu");
@@ -249,7 +319,7 @@ $(document).ready(function(){
     });
 
     startButton.click(function(){
-        if (gameOn === true) {
+        if (gameOn === true || audioPlaying === true || audioPlaying === true) {
             return false;
         };
         gameOn = true;
@@ -262,7 +332,7 @@ $(document).ready(function(){
     });
 
     lossPlayAgainButton.click(function(){
-        if (gameOn === true) {
+        if (gameOn === true || audioPlaying === true) {
             return false;
         };
         const hiddenMenuZone = $("#hiddenMenu");
@@ -273,7 +343,7 @@ $(document).ready(function(){
     });
 
     winPlayAgainButton.click(function(){
-        if (gameOn === true) {
+        if (gameOn === true || audioPlaying === true) {
             return false;
         };
         const hiddenMenuZone = $("#hiddenMenu");
@@ -340,5 +410,65 @@ $(document).ready(function(){
             }
         }
     };
+
+    //object constructor to handle sound objects. This came directly from w3school's sudio example for HTML5 audio element creation using a function. Source: "https://www.w3schools.com/graphics/game_sound.asp"
+    function sound(src) {
+        this.sound = document.createElement("audio");
+        this.sound.src = src;
+        this.sound.setAttribute("preload", "auto");
+        this.sound.setAttribute("controls", "none");
+        this.sound.style.display = "none";
+        document.body.appendChild(this.sound);
+        this.play = function(){
+            this.sound.play();
+        }
+        this.stop = function(){
+            this.sound.pause();
+        }
+    } 
+
+    //this event capturing bit of code comes from the Webshim Library at "http://afarkas.github.io/webshim/demos/index.html" through saml's answer to a question on stackoverflow at "https://stackoverflow.com/questions/11291651/why-dont-audio-and-video-events-bubble"
+    $.createEventCapturing = (function () {
+        var special = $.event.special;
+        return function (names) {
+            if (!document.addEventListener) {
+                return;
+            }
+            if (typeof names == 'string') {
+                names = [names];
+            }
+            $.each(names, function (i, name) {
+                var handler = function (e) {
+                    e = $.event.fix(e);
+
+                    return $.event.dispatch.call(this, e);
+                };
+                special[name] = special[name] || {};
+                if (special[name].setup || special[name].teardown) {
+                    return;
+                }
+                $.extend(special[name], {
+                    setup: function () {
+                        this.addEventListener(name, handler, true);
+                    },
+                    teardown: function () {
+                        this.removeEventListener(name, handler, true);
+                    }
+                });
+            });
+        };
+    })();
+
+    //function to change audio playing state to true, which will be used to prevent button presses during audio.
+    $.createEventCapturing(['play']);  
+    jQuery('body').on('play', 'audio', function(){
+        audioPlaying = true;
+    });
+
+        //function to change audio playing state to false, which will be used to allow button presses again.
+    $.createEventCapturing(['ended']);  
+    jQuery('body').on('ended', 'audio', function(){
+        audioPlaying = false;
+    });
 });
 
